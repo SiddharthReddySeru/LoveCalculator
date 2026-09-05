39a2116c078cbf9ad48018c93173a920891bd110
Seru Siddharth Reddy <siddharthreddyseru@gmail.com>
2026-09-05 15:33:08 +0530

    Update FORMSPREE_URL to new endpoint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- 1. CONFIGURATION ---
-const FORMSPREE_URL = 'https://formspree.io/f/mblzyaop'; 
+const FORMSPREE_URL = 'https://formspree.io/f/mbgjkezv'; 
 
 let button = document.querySelector('.calc');
 const h1Result = document.querySelector('h1');
